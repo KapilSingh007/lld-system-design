611fa592c1aafd4368ed289b68e9f58b24cfbf02
KapilSingh007 <kapilsingh3467@gmail.com>
2026-06-10 23:30:09 +0530

    singleton design pattern method best  method

--- singleton-strategy/singleton.cpp
+++ singleton-strategy/singleton.cpp
@@ -1,28 +1,33 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
 class SingletonClass {
+private:
+    SingletonClass() {
+        std::cout << "SingletonClass created\n";
+    }
 
-    private:
-    static SingletonClass *instance;
-    SingletonClass(){}
+    // Prevent copyingƒ
+    SingletonClass(const SingletonClass&) = delete;
+    SingletonClass& operator=(const SingletonClass&) = delete;
 
-    public:
-    static SingletonClass *getClassInstance(){
-        if(instance) {
-            return instance;
-        } else {
-            instance = new SingletonClass();
-            return instance;
-        }
+public:
+    static SingletonClass& getInstance() {
+        static SingletonClass instance;  // Created once, thread-safe since C++11
+        return instance;
+    }
+
+    void showMessage() {
+        std::cout << "Hello from SingletonClass\n";
     }
 };
-SingletonClass* SingletonClass::instance = nullptr;
 
-int main(){
-    SingletonClass *instance1 = SingletonClass::getClassInstance();
-    SingletonClass *instance2 = SingletonClass::getClassInstance();
+int main() {
+    SingletonClass& s1 = SingletonClass::getInstance();
+    SingletonClass& s2 = SingletonClass::getInstance();
 
-    cout<<"is class instance is same: "<<(instance1 == instance2);
+    s1.showMessage();
+
+    std::cout << (&s1 == &s2) << std::endl; // 1 (true)
+
     return 0;
 }
