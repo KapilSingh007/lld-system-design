c03ac78bde246aed5a52c60beb6f3c46ba40cfff
KapilSingh007 <kapilsingh3467@gmail.com>
2026-07-14 10:52:14 +0530

    remote-design-pattern

---
+++ proxy-design-pattern/remote-design-pattern.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class IDataFetch{
+    public:
+    virtual void fetch() = 0;
+
+};
+class RealDataService: public IDataFetch{
+    
+    string imagePath;
+    public:
+    RealDataService(){
+        cout << "[RealDataService] Initialized (simulating remote setup)\n";
+    }
+
+    void fetch() override {
+        cout<<"[RealDataService] Data from server";
+    }
+};
+class ProxyDataService: public IDataFetch{
+    IDataFetch* data;
+
+    public:
+
+    void fetch() override {
+        if(!data){
+            data = new RealDataService();
+        }
+        data->fetch();
+    }
+
+};
+
+int main(){
+
+    IDataFetch* dataService  = new ProxyDataService();
+    dataService->fetch();
+}
